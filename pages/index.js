22b145850959f934f8541e26dff82fc8eed3b29c
igormoreira-computer.eng <igor_moreira@live.com>
2020-12-15 21:51:54 -0300

    upadate

--- pages/index.js
+++ pages/index.js
@@ -8,33 +8,36 @@ const Index = (props) => {
     return (
         // key -> identificação do repositório
         <div className='container mx-auto '>
-            <div className='grid grid-cols-4 pt-16 leading-none '>               
-                <div className=' col-start-2 col-span-2 pt-30 '> 
-                    <h1 className='font-medium text-2xl tracking-widest'>Hi, I'm</h1>
-                    <h1 className='font-display text-5xl tracking-wider'>Igor Moreira</h1>
-                    <h1 className='font-bold text-2xl tracking-widest'>Computer Engineer</h1>
-                    <div className='pt-20'>
-                        <div className='shadow-lg border-current'>
-                            <h1 className='text-indigo-600 group-hover:text-gray-900'>Contact me</h1>
-                            <h1>Linkedin</h1>
-                            <h1>Github</h1>
+            <div className='grid grid-cols-4 pt-16 leading-none '>                            
+                <div className=' col-start-2 col-span-2 pt-30 '>
+                    <div className='pt-48'> 
+                        <h1 className='font-medium text-2xl tracking-widest'>Hi, I'm</h1>
+                        <h1 className='font-display text-5xl tracking-wider'>Igor Moreira</h1>
+                        <h1 className='font-bold text-2xl tracking-widest'>Computer Engineer</h1>
+                        <div className='pt-20'>
+                            <div className='shadow-lg border-current'>
+                                <h1 className=''>Contact me</h1>
+                                <h1>Linkedin</h1>
+                                <h1>Github</h1>
+                            </div>
                         </div>
-                    </div>
+                    </div>                    
                 </div>
-
                 <div className=' col-start-7 col-span-2'><img src ='/images/Igor-Moreira.png'/></div>
             </div>
             
-            <div className=' bg-gradient-to-r  from-current via-primary to-current bg-opacity-25 rounded-full py-3 px-10 tracking-widest '>                 
-                    <h1 className='opacity-100 uppercase text-center font-display text-gray-900 text-6xl'>what i do</h1>
-                    <div className='flex items-center justify-center py-5 text-4xl mt-5 mb-10 leading-8 text-white font-medium'>
-                        <h1 className='pr-8 pl-8 hover:transparent'>FullStack Developer</h1>
+            <div className='  rounded-full py-3 tracking-widest shadow-2xl'>                 
+                <div className=''>
+                    <h1 className='opacity-100 uppercase text-center font-display text-gray-900 text-5xl'>what i do</h1>
+                    <div className='flex items-center justify-center p-32 py-5 text-3xl mt-5 mb-10 leading-8 text-white font-medium'>
+                        <h1 className='pr-8 pl-8 '>FullStack Developer</h1>
                         <h1 className='pr-8 pl-8 '>Computer Engineer</h1>
                         <h1 className='pr-8 pl-8'>Marketing Enthusiast</h1>
                     </div>
+                </div>
             </div>
             
-            <h2 className='font-display text-4xl text-center'>Meus repositórios no github</h2>
+            <h2 className='font-display text-4xl text-center pt-8 uppercase'>TECH CONTRIBUTIONS</h2>
             <p className='font-medium text-xl'>Github stats: public_repos: {props.user.public_repos} </p>            
             {props.repos.map(repo => {
                 return(
