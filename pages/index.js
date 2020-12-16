ee9ea6a78aa1a5d285d5ac73cd2f90efadc33514
igormoreira-computer.eng <igor_moreira@live.com>
2020-12-16 14:36:05 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -7,63 +7,64 @@ const Index = (props) => {
 //const Index = ({repos,user}) => {
     return (
         // key -> identificação do repositório
-        <div className='bg-network'> 
+        <div className='bg-network '> 
+            <div className='container mx-auto'>
                    
-            <div className='grid grid-cols-4 pt-16 leading-none '>                            
-                <div className=' col-start-2 col-span-2 pt-30 '>
-                    <div className='pt-48'> 
-                        <h1 className='font-medium text-2xl tracking-widest '>Hi, I'm</h1>
-                        <h1 className='font-display text-5xl tracking-wider'>Igor Moreira</h1>
-                        <h1 className='font-bold text-2xl tracking-widest '>Computer Engineer</h1>
-                        <div className='pt-20'>
-                            <div className='shadow-lg border-current'>
-                                <h1 className=''>Contact me</h1>
-                                <h1>Linkedin</h1>
-                                <h1>Github</h1>
+                <div className='grid grid-cols-4 pt-16 leading-none '>                            
+                    <div className=' col-start-2 col-span-2 pt-30 '>
+                        <div className='pt-48'> 
+                            <h1 className='font-medium text-2xl tracking-widest '>Hi, I'm</h1>
+                            <h1 className='font-display text-5xl tracking-wider'>Igor Moreira</h1>
+                            <h1 className='font-bold text-2xl tracking-widest '>Computer Engineer</h1>
+                            <div className='pt-20'>
+                                <div className='shadow-lg border-current'>
+                                    <h1 className=''>Contact me</h1>
+                                    <h1>Linkedin</h1>
+                                    <h1>Github</h1>
+                                </div>
                             </div>
+                        </div>                    
+                    </div>
+                    <div className=' col-start-7 col-span-2'><img src ='/images/Igor-Moreira.png'/></div>
+                </div>
+            
+                <div className=' simple-linear  rounded-full py-3 tracking-widest shadow-2xl'>                 
+                    <div className=''>
+                        <h1 className='uppercase text-center font-display text-gray-900 text-5xl'>what i do</h1>
+                        <div className='flex items-center justify-center  py-5 text-3xl mt-5 mb-10 leading-8 text-blue-900  font-bold'>
+                            <h1 className='pr-8 pl-8 '>FullStack Developer</h1>
+                            <h1 className='pr-8 pl-8 '>Computer Engineer</h1>
+                            <h1 className='pr-8 pl-8'>Marketing Enthusiast</h1>
                         </div>
-                    </div>                    
-                </div>
-                <div className=' col-start-7 col-span-2'><img src ='/images/Igor-Moreira.png'/></div>
-            </div>
-        
-            <div className=' simple-linear  rounded-full py-3 tracking-widest shadow-2xl'>                 
-                <div className=''>
-                    <h1 className='uppercase text-center font-display text-gray-900 text-5xl'>what i do</h1>
-                    <div className='flex items-center justify-center  py-5 text-3xl mt-5 mb-10 leading-8 text-blue-900  font-bold'>
-                        <h1 className='pr-8 pl-8 '>FullStack Developer</h1>
-                        <h1 className='pr-8 pl-8 '>Computer Engineer</h1>
-                        <h1 className='pr-8 pl-8'>Marketing Enthusiast</h1>
                     </div>
                 </div>
-            </div>
-            
-            <div>
-                <h1 className='uppercase text-center font-display text-gray-900 text-5xl tracking-widest pt-16'>my education</h1>
-                <h1 className='pt-16'></h1>
-            </div>
-           
-            <div className='flex items-center justify-center '>
-                <div className='bg-gradient-to-r from-transparent via-primary shadow-2xl ellipse flex items-center justify-center '>                 
-                    <div className='text-center leading-8 text-blue-900 uppercase tracking-widest'>
-                        <h1 className='text-xl font-bold'>graduation degree</h1>
-                        <h1 className='pr-8 pl-8 font-regular text-3xl pt-6'>Computer Engineer</h1>
-                        <h1 className='pr-8 pl-8 pt-5 font-bold text-xl pt-6'>ufu - university of uberlândia</h1>
-                    </div>
+                
+                <div>
+                    <h1 className='uppercase text-center font-display text-gray-900 text-5xl tracking-widest pt-16'>my education</h1>
+                    <h1 className='pt-16'></h1>
                 </div>
-            </div>                     
             
-            <h2 className='font-display text-4xl text-center pt-8 uppercase'>TECH CONTRIBUTIONS</h2>
-            <p className='font-medium text-2xl mx-8'>Github stats: public_repos: {props.user.public_repos} </p>            
-            {props.repos.map(repo => {
-                return(
-                    <div key ={repo.id} className='text-xl rounded bg-gray-300 bg-opacity-75 mx-8 my-4 p-4 hover:shadow-md'>
-                        <h3 className='font-bold'>{repo.full_name}</h3>
-                        <p>Language: {repo.language}</p>
-                        
-                    </div>)
-            })}
-           
+                <div className='flex items-center justify-center '>
+                    <div className='bg-gradient-to-r from-transparent via-primary shadow-2xl ellipse flex items-center justify-center '>                 
+                        <div className='text-center leading-8 text-blue-900 uppercase tracking-widest'>
+                            <h1 className='text-xl font-bold'>graduation degree</h1>
+                            <h1 className='pr-8 pl-8 font-regular text-3xl pt-6'>Computer Engineer</h1>
+                            <h1 className='pr-8 pl-8 pt-5 font-bold text-xl pt-6'>ufu - university of uberlândia</h1>
+                        </div>
+                    </div>
+                </div>                     
+                
+                <h2 className='font-display text-4xl text-center pt-8 uppercase'>TECH CONTRIBUTIONS</h2>
+                <p className='font-medium text-2xl mx-8'>Github stats: public_repos: {props.user.public_repos} </p>            
+                {props.repos.map(repo => {
+                    return(
+                        <div key ={repo.id} className='text-xl rounded bg-gray-300 bg-opacity-75 mx-8 my-4 p-4 hover:shadow-md'>
+                            <h3 className='font-bold'>{repo.full_name}</h3>
+                            <p>Language: {repo.language}</p>
+                            
+                        </div>)
+                })}
+            </div>
         </div>
     )
 }
