0e5fa90d7941493d324b0c9fb33512d188abe14f
igormoreira-computer.eng <igor_moreira@live.com>
2020-12-17 15:12:16 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const Index = (props) => {
                             <div className='relative border-primary border rounded  py-4 mt-6 pt-10 shadow-lg'>
                                 <h1 className='absolute rounded-lg bg-primary py-2 px-6 top-0 mx-16 -mt-5 text-white text-xl font-bold uppercase'>Contact me</h1>
                                 <ul className='flex items-center justify-center  text-4xl mt-2 mb-5'>
-                                    <li  className='pr-8 pl-8'><a href=''><IoLogoWhatsapp /></a></li>
+                                    <li  className='pr-8 pl-8'><a href='https://api.whatsapp.com/send?phone=553498365664'><IoLogoWhatsapp /></a></li>
                                     <li  className='pr-8 pl-8'><a href='https://www.linkedin.com/in/igormoreiraufu/'><AiFillLinkedin /></a></li>
                                     <li  className='pr-8 pl-8'><a href='https://github.com/Igor-Moreira'><FaGithub /></a></li>
                                 </ul>
