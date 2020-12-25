7206b11ead4bf1af209c3ff822808bb73b7fa17f
igormoreira-computer.eng <igor_moreira@live.com>
2020-12-25 18:50:53 -0300

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,15 @@
-module.exports = {
+const purgecss = [
+    "@fullhuman/postcss-purgecss",
+    {
+      content: ["./components/**/*.js", "./pages/**/*.js"],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    }
+  ];
+  module.exports = {
     plugins: [
-        'tailwindcss',
-        process.env.NODE_ENV === 'production'
-            ? [
-                '@fullhuman/postcss-purgecss',
-                {
-                    content: [
-                        './pages/**/*.{js,jsx,ts,tsx}',
-                        './components/**/*.{js,jsx,ts,tsx}',
-                    ],
-                    defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
-                },
-            ]
-            : undefined,
-        'autoprefixer'
-    ],
-}
+      "postcss-import",
+      "tailwindcss",
+      "autoprefixer",
+      ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    ]
+  };
