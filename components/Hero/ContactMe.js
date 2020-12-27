cdccfc684ddaae2e9e7f34be760255241247ae36
igormoreira-computer.eng <igor_moreira@live.com>
2020-12-27 18:35:05 -0300

    update

--- components/Hero/ContactMe.js
+++ components/Hero/ContactMe.js
@@ -5,7 +5,6 @@ import { FaGithub } from 'react-icons/fa';
 
 const ContactMe = () => {
     return(
-        <div className=''>
             <div className='relative border-primary border rounded  py-4 mt-6 pt-10 shadow-lg'>
                 <h1 className='absolute rounded-lg bg-primary py-2 px-6 top-0 mx-16 -mt-5 text-white text-xl font-bold uppercase'>Contact me</h1>
                 <ul className='flex items-center justify-center  text-4xl mt-2 mb-5'>
@@ -14,8 +13,7 @@ const ContactMe = () => {
                     <li  className='pr-8 pl-8'><a href='https://github.com/Igor-Moreira'><FaGithub /></a></li>
                 </ul>
                 <h1 className='flex items-center justify-center'> or drop a line: igor_moreira@live.com </h1>                                                            
-            </div>
-        </div>
+            </div>        
     )
 }
 export default ContactMe
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import getUser from '../utils/getUser'
 import {RiGitRepositoryLine} from 'react-icons/ri'
 import PageHead from '../components/PageHead';
 import Hero from '../components/Hero';
+import Summary from '../components/Summary';
 
 
 //Front-end
@@ -14,20 +15,9 @@ const Index = (props) => {
         //<div className='bg-network'> 
         <div className='bg-network container mx-auto body'> 
             <PageHead/>            
-            <Hero/>     
+            <Hero/> 
+            <Summary/>
 
-            
-            <div className=' simple-linear  rounded-full py-3 tracking-widest shadow-2xl'>                 
-                <div className=''>
-                    <h1 className='uppercase text-center font-display text-gray-900 text-5xl'>what i do</h1>
-                    <div className='flex items-center justify-center divide-x divide-primary text-sm px-5 md:py-5 md:text-2xl mt-5 mb-10 leading-8 text-blue-900 font-bold uppercase'>
-                        <h2 className='md:pr-5 md:pl-5 pr-2 pl-2'>FullStack Developer</h2>
-                        <h2 className='md:pr-5 md:pl-5 pr-2 pl-2'>Computer Engineer</h2>
-                        <h2 className='md:pr-5 md:pl-5 pr-2 pl-2'>Marketing Enthusiast</h2>
-                    </div>
-                </div>
-            </div>
-            
             <div>
                 <h1 className='uppercase text-center font-display text-gray-900 text-5xl tracking-widest pt-16'>my education</h1>
                 <h1 className='pt-16'></h1>
