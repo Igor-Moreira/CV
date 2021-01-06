f63bf5b3ede39a02b90ee660807db2a7d368b8cf
igormoreira-computer.eng <igor_moreira@live.com>
2021-01-06 19:07:24 -0300

    update

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Footer = () =>{
+const Footer = ({ currentDate }) =>{
     return(
         <div>
-            <p className='font-sans text-center py-12 '>You can find the source-code of this website:
+            <p className='font-sans text-center py-12 '>I used NextJS + SSR (Server Side Rendering) + Vercel (as platform)
+            <br /> You can find the source-code of this website:
             <br />
             <a className='font-sans font-bold hover:underline' href='https://github.com/Igor-Moreira/CV'>https://github.com/Igor-Moreira/CV</a>
             </p>
